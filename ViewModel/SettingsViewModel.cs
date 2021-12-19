f5bce347ee59ec7d762d595a1db16791e9c375bf
Atakan Cesim <atakan.cesim@matriksdata.com>
2021-12-20 00:54:28 +0300

    ayarlar sayfası geliştiriliyor

--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -1,9 +1,51 @@
-﻿using ModbusRecorder.Utils;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using ModbusRecorder.Annotations;
+using ModbusRecorder.Utils;
 
 namespace ModbusRecorder.ViewModel
 {
-    public class SettingsViewModel:BaseViewModel
+    public class SettingsViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        private string _selectedConnection;
+        private ObservableCollection<string> _connections;
 
+        public string SelectedConnection
+        {
+            get => _selectedConnection;
+            set
+            {
+                if (value == _selectedConnection) return;
+                _selectedConnection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Connections
+        {
+            get => _connections;
+            set
+            {
+                _connections = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SettingsViewModel()
+        {
+            Task.Run(() => Connections = new ObservableCollection<string>(SerialPort.GetPortNames()));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
